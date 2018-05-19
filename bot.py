3522380756f321098169a547c83ce22f4e57c27c
KaIamatas <36992600+KaIamatas@users.noreply.github.com>
2018-05-19 22:57:08 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,24 @@
 import discord
-from discord.ext import commands
 
-bot = commands.Bot(command_prefix='$')
+TOKEN = 'NDQ3NTAwMDAzOTIyMTQ5Mzc3.DeIeaQ.USPt4Bsnj0EagijHaIrwqZUsyo0'
 
-@bot.event
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+
+@client.event
 async def on_ready():
     print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
+    print(client.user.name)
+    print(client.user.id)
     print('------')
 
-@bot.command()
-async def greet(ctx):
-    await ctx.send(":smiley: :wave: Hello, there!")
-
-bot.run('NDQ2NTEwMTQ5MjQzMzcxNTMw.Dd6FFQ.FsAfmlvkujGfL7rTZG8YwSrwwFM')
+client.run(TOKEN)
